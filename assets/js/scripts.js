8253a88e2decd317d5d10028efe12e1d3675cd55
Renan Souza <renandesouza1102@gmail.com>
2024-11-04 19:06:57 -0300

    feat: add seção Sobre e Finalizando

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,3 +26,12 @@ accordionHeaders.forEach((header) => {
       : accordionItem.classList.add("active");
   });
 });
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
